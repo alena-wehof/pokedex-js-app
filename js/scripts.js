f5f822c76026302e402051a42ca1a06506be36d3
alena-wehof <130425488+alena-wehof@users.noreply.github.com>
2023-05-20 15:41:54 -0400

    Added loop to write list on page
    Writes names and heights, checks height to add an extra note if it's big

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
     { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
 ];
+
+//list pokemon on page with their heights in parentheses
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p></p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    //check the height to add extra note
+    if (pokemonList[i].height > 1.5) {
+        document.write(' - Wow, that\'s big!');
+    }
+}
