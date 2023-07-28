3e82b3d0573acb3ecf426c8b25c6464c403b5639
alena-wehof <alena.wehof@gmail.com>
2023-07-28 15:52:41 -0400

    Added IIFE around pokemonList
    Also updated  forEach loop to work with variables from the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-    { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+        { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 //list pokemon on page with their heights in parentheses
-pokemonList.forEach(function (pokemonList) {
-    document.write('<p></p>' + pokemonList.name + ' (height: ' + pokemonList.height + ')');
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write('<p></p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
     //check the height to add extra note
-    if (pokemonList.height > 1.5) {
+    if (pokemon.height > 1.5) {
         document.write(' - Wow, that\'s big!');
     }
 })
