4bee3a54d732ba695c93cd333a581d6b1ddfc34d
alena-wehof <130425488+alena-wehof@users.noreply.github.com>
2023-08-03 16:54:27 -0400

    Create the addListItem function in the IIFE
    Moved code from the forEach loop into the new function in the IIFE, then called that function to list the buttons on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,37 @@ let pokemonRepository = (function () {
         { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
     ];
 
+    //add a pokemon object to the pokemonList
     function add(pokemon) {
         if (typeof pokemon === "object") {
             pokemonList.push(pokemon);
         }
     }
 
+    //access the full list of pokemon
     function getAll() {
         return pokemonList;
     }
 
+    //create a button with the pokemon's name on it
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('li');
+        let pokemonButton = document.createElement('button');
+        pokemonButton.innerText = pokemon.name;
+        pokemonButton.classList.add('pokemon');
+        listPokemon.appendChild(pokemonButton);
+        pokemonList.appendChild(listPokemon);
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 })();
 
-//create a list of buttons so each pokemon's name is on its own button
+//create a list of pokemon buttons on the page
 pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listPokemon = document.createElement('li');
-    let pokemonButton = document.createElement('button');
-    pokemonButton.innerText = pokemon.name;
-    pokemonButton.classList.add('pokemon');
-    listPokemon.appendChild(pokemonButton);
-    pokemonList.appendChild(listPokemon);
-})
+    pokemonRepository.addListItem(pokemon);
+});
